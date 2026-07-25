f42e42d5cecada9ce1bf54dcbd44ec51deb1225d
Gautam Krishna Balgi <121880225+gautambalgi@users.noreply.github.com>
2026-07-25 15:14:50 +0000

    remove text loop for now

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,4 @@
 import TextEffect from "../components/TextEffect";
-import TextLoop from "../components/TextLoop";
-
-const roles = [
-  "Data Scientist",
-  "Machine Learning",
-  "Artificial Intelligence",
-  "Data Analyst",
-  "Business Analyst",
-];
 
 export default function Home() {
   return (
@@ -16,9 +7,6 @@ export default function Home() {
         <h1 className="home-name">
           <TextEffect text="Gautam Krishna Balgi" />
         </h1>
-        <div className="home-roles">
-          <TextLoop items={roles} className="gradient-text" interval={2200} />
-        </div>
       </div>
     </main>
   );
